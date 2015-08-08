d8dc3263bffb3fc15c513e2b6527b2820e83708d
Hao <orctom@gmail.com>
2015-08-08 23:52:48 +0800

    dummy proxy

---
+++ laputa-client/src/main/java/com/orctom/laputa/internal/ServiceInterceptor.java
@@ -0,0 +1,7 @@
+package com.orctom.laputa.internal;
+
+/**
+ * Created by hao on 6/15/15.
+ */
+public class ServiceInterceptor {
+}
---
+++ laputa-client/src/test/java/com/orctom/laputa/service/DummyModel.java
@@ -0,0 +1,37 @@
+package com.orctom.laputa.service;
+
+public class DummyModel {
+
+	public DummyModel() {
+	}
+
+	public DummyModel(String name, String desc) {
+		this.name = name;
+		this.desc = desc;
+	}
+
+	private String name;
+	private String desc;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return "<<[name: " + name + ", desc: " + desc + "]>>";
+	}
+
+}
