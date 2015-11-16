1844b85a5972ece80ca444505c3b8666cd8385e6
Hao <orctom@gmail.com>
2015-11-16 23:45:29 +0800

    use guava reflections

--- laputa-client/src/main/java/com/orctom/laputa/Laputa.java
+++ laputa-client/src/main/java/com/orctom/laputa/Laputa.java
@@ -3,16 +3,15 @@ package com.orctom.laputa;
 import java.util.HashMap;
 import java.util.Map;
 
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.matcher.ElementMatchers;
+import com.google.common.reflect.Reflection;
+import com.orctom.laputa.internal.ServiceInterceptor;
 
 import com.orctom.laputa.annotation.LaputaService;
 import com.orctom.laputa.util.AnnotationUtils;
 import com.orctom.laputa.util.ServiceRegistryUtils;
 
 /**
+ * Client
  * Created by hao on 4/28/15.
  */
 public class Laputa {
@@ -20,18 +19,7 @@ public class Laputa {
 	private static Map<String, String> SERVICE_URL_MAPPING = new HashMap<>();
 
 	public static <T> T instrument(Class<T> interfaceClass) {
-		Class<? extends T> dynamicType = new ByteBuddy()
-				.subclass(interfaceClass)
-				.method(ElementMatchers.isMethod())
-				.intercept(MethodDelegation.to(new ServiceInterceptor()))
-				.make()
-				.load(getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
-				.getLoaded();
-		try {
-			return dynamicType.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			throw new UnsupportedOperationException(e);
-		}
+		return Reflection.newProxy(interfaceClass, new ServiceInterceptor());
 	}
 
 	private static ClassLoader getClassLoader() {
--- laputa-client/src/main/java/com/orctom/laputa/internal/ServiceInterceptor.java
+++ laputa-client/src/main/java/com/orctom/laputa/internal/ServiceInterceptor.java
@@ -1,7 +1,17 @@
 package com.orctom.laputa.internal;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
 /**
+ * InvocationHandler for proxy
  * Created by hao on 6/15/15.
  */
-public class ServiceInterceptor {
+public class ServiceInterceptor implements InvocationHandler {
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		System.out.println("InvocationHandler " + method.toString());
+		return null;
+	}
 }
