0ce9625f5f932f1af519dadcec9b52883bc63998
orctom <orctom@gmail.com>
2016-11-30 11:03:24 +0800

    a simple counter

---
+++ laputa-utils/src/main/java/com/orctom/laputa/utils/SinpleCounter.java
@@ -0,0 +1,45 @@
+package com.orctom.laputa.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple counter ¯\_(ツ)_/¯
+ * Created by chenhao on 11/30/16.
+ */
+public class SinpleCounter {
+
+  private Map<String, MutableInt> counters = new HashMap<>();
+
+  public int count(String key) {
+    return getConter(key).increase();
+  }
+
+  public int count(String key, int increment) {
+    return getConter(key).increaseBy(increment);
+  }
+
+  private MutableInt getConter(String key) {
+    MutableInt counter = counters.get(key);
+    if (null != counter) {
+      return counter;
+    }
+
+    counter = new MutableInt(0);
+    synchronized (this) {
+      MutableInt old = counters.put(key, counter);
+      if (null != old) {
+        counter.increaseBy(old.getValue());
+      }
+    }
+    return counter;
+  }
+
+  public Map<String, MutableInt> getCounters() {
+    return counters;
+  }
+
+  public void reset() {
+    counters = new HashMap<>();
+  }
+}
