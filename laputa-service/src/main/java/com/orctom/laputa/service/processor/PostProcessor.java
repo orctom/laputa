c2af27704d6abfa3995aa8ef68db8d7e5bda83c5
Hao <orctom@gmail.com>
2017-04-07 18:44:52 +0800

    refactored

--- laputa-service/src/main/java/com/orctom/laputa/service/processor/PostProcessor.java
+++ laputa-service/src/main/java/com/orctom/laputa/service/processor/PostProcessor.java
@@ -1,5 +1,7 @@
 package com.orctom.laputa.service.processor;
 
+import com.orctom.laputa.service.model.Context;
+
 /**
  * processors before translate the Java object to endpoint (json, xml, html...)
  * They will be process by order of <code>getOrder()</code> ASC
@@ -9,5 +11,5 @@ public interface PostProcessor {
 
   int getOrder();
 
-  Object process(Object data);
+  Object process(Object data, Context ctx);
 }
